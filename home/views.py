1f64a68ce81c27de04106b57edf404345a9c5fff
Umang <umang.prajapati1999@gmail.com>
2019-09-24 20:31:08 +0530

    Home Page

--- home/views.py
+++ home/views.py
@@ -1,25 +1,6 @@
 from django.shortcuts import render
 from .models import Product
 # Create your views here.
-products = [
-    {
-        'name': 'RTX 2060 Super',
-        'type': 'Graphic Card',
-        'price': '37000',
-    },
-    {
-        'name': 'RTX 2060',
-        'type': 'Graphic Card',
-        'price': '32000',
-    },
-    {
-        'name': 'Samsung Evo 970 512GB NvMe SSD',
-        'type': 'SSD',
-        'price': '9000',
-    }
-]
-
-
 def home(request):
     context = {
         'products': Product.objects.all()
