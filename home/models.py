851feb536629d635b83969d796cc29b1cfe6487f
Ajinkya Kudoo <ajinkyakudoo@gmail.com>
2019-09-24 18:29:31 +0530

    Product Model Modified

--- home/models.py
+++ home/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField(max_length=10)
     stock = models.IntegerField()
-    type = models.CharField(max_length=50,default="Hardware")
+    type = models.CharField(max_length=50, default="Hardware")
     description = models.TextField()
     avg_rating = models.FloatField(max_length=10, default=0.0)
+    image = models.ImageField(default="default.jpg", upload_to="product-pics")
